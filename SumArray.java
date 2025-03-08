aa8d34ea9a8b36f582a40d714d5c69e212722770
NavyaJampana <jnavya2024@gmail.com>
2025-03-08 16:48:47 +0530

    Add files via upload

---
+++ SumArray.java
@@ -0,0 +1,15 @@
+package Arrays;
+public class SumArray {
+    public static int sum(int[] arr) {
+        int total = 0;
+        for (int num : arr) {
+            total += num;
+        }
+        return total;
+    }
+
+    public static void main(String[] args) {
+        int[] numbers = {1, 2, 3, 4, 5};
+        System.out.println("Sum: " + sum(numbers));
+    }
+}
